166cce591af2b32f4a9de74c4b76ea7afb51e3c1
blitzrage <blitzrage@n-visibleinc.com>
2015-01-31 10:00:00 -0500

    Clean up and renaming
    * renamed functions and channels to be more clear
    * ran through go fmt
    * some setup for next steps

--- go-ari-client.go
+++ go-ari-client.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "io/ioutil"
-  "math/rand"
-  "os"
-  "os/signal"
-  "syscall"
-  "time"
-  "encoding/json"
-  "log"
-  "strconv"
-  
-  "go-ari-library"
-  "github.com/bitly/go-nsq"
-  "github.com/bitly/nsq/util"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/bitly/go-nsq"
+	"github.com/bitly/nsq/util"
+	"go-ari-library"
 )
 
 var (
-  config Config
-  totalMessages int = 0
-  maxInFlight int = 200
+	config        Config
+	totalMessages int = 0
+	maxInFlight   int = 200
 )
 
 type Config struct {
-	LookupdHttpAddress   []string `json:"lookupd_http_address"`
-	Application          string `json:"application"`            // aka Topic
-  Channel              string `json:"channel"`
-  MaxInFlight          string `json:"max_in_flight"`
-  TotalMessages        string `json:"total_messages"`
+	LookupdHttpAddress []string `json:"lookupd_http_address"`
+	Application        string   `json:"application"` // aka Topic
+	Channel            string   `json:"channel"`
+	MaxInFlight        string   `json:"max_in_flight"`
+	TotalMessages      string   `json:"total_messages"`
 }
 
 type MessageHandler struct {
@@ -38,9 +38,9 @@ type MessageHandler struct {
 }
 
 func init() {
-  var err error
+	var err error
 
-  // parse the configuration file and get data from it
+	// parse the configuration file and get data from it
 	configpath := flag.String("config", "./config_client.json", "Path to config file")
 	flag.Parse()
 	configfile, err := ioutil.ReadFile(*configpath)
@@ -53,83 +53,101 @@ func init() {
 }
 
 func GenHandler() (func(*nsq.Message) error, chan []byte) {
-  in := make(chan []byte)
-  return func(m *nsq.Message) error {
-    in <- m.Body
-    return nil
-  }, in
+	in := make(chan []byte)
+	return func(m *nsq.Message) error {
+		in <- m.Body
+		return nil
+	}, in
 }
 
-func ProcessMessage(out chan *nv.NV_Event) {
-  for outmessage := range out {
-    switch outmessage.Type {
-    case "StasisStart":
-      fmt.Println("Got start message")
-    case "ChannelDtmfReceived":
-      fmt.Println("Got DTMF")
-    case "ChannelHangupRequest":
-      fmt.Println("Channel hung up")
-    case "StasisEnd":
-      fmt.Println("Got end message")
-    }  
-  }
+func PublishCommand(channel string, command *nv.NV_Command, p *nsq.Producer) {
+	busMessage, _ := json.Marshal(command)
+
+	fmt.Printf("[DEBUG] Bus Data for %s:\n%s", channel, busMessage)
+	p.Publish(channel, []byte(busMessage))
+}
+
+func ConsumeEvents(events chan *nv.NV_Event) {
+	for event := range events {
+		switch event.Type {
+		case "StasisStart":
+			fmt.Println("Got start message")
+		case "ChannelDtmfReceived":
+			fmt.Println("Got DTMF")
+		case "ChannelHangupRequest":
+			fmt.Println("Channel hung up")
+		case "StasisEnd":
+			fmt.Println("Got end message")
+		}
+	}
 }
 
 func main() {
-  fmt.Println("Welcome to the go-ari-client")
-  
-  if config.Application == "" {
-    log.Fatal("Missing Application configuration")
-  }
-  
-  if len(config.LookupdHttpAddress) == 0 {
-    log.Fatal("Missing Lookupd HTTP Address configuration")
-  }
-  
-  if config.Channel == "" {
-    rand.Seed(time.Now().UnixNano())
-    config.Channel = fmt.Sprintf("tail%06d#ephemeral", rand.Int()%999999)
-  }
+	fmt.Println("Welcome to the go-ari-client")
 
-  
-  if config.MaxInFlight != "" {
-    maxInFlight, _ = strconv.Atoi(config.MaxInFlight)
-  }
-  
-  if config.TotalMessages != "" {
-    totalMessages, _ = strconv.Atoi(config.TotalMessages)
-  }
-  
-  sigChan := make(chan os.Signal, 1)
+	// initial setup
+	if config.Application == "" {
+		log.Fatal("Missing Application configuration")
+	}
+
+	if len(config.LookupdHttpAddress) == 0 {
+		log.Fatal("Missing Lookupd HTTP Address configuration")
+	}
+
+	if config.Channel == "" {
+		rand.Seed(time.Now().UnixNano())
+		config.Channel = fmt.Sprintf("tail%06d#ephemeral", rand.Int()%999999)
+	}
+
+	if config.MaxInFlight != "" {
+		maxInFlight, _ = strconv.Atoi(config.MaxInFlight)
+	}
+
+	if config.TotalMessages != "" {
+		totalMessages, _ = strconv.Atoi(config.TotalMessages)
+	}
+
+	// listen for exit
+	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-  
-  // Don't ask for more messages than we want
+
+	// Don't ask for more messages than we want
 	if totalMessages > 0 && totalMessages < maxInFlight {
 		maxInFlight = totalMessages
 	}
 
-  // connect to nsq and get the json then save to a value  
-  cfg := nsq.NewConfig()
-  cfg.UserAgent = fmt.Sprintf("go_ari_client/%s go-nsq/%s", util.BINARY_VERSION, nsq.VERSION)
-  cfg.MaxInFlight = maxInFlight
-  
-  consumer, err := nsq.NewConsumer(config.Application, config.Channel, cfg)
+	// connect to nsq and get the json then save to a value
+	cfg := nsq.NewConfig()
+	cfg.UserAgent = fmt.Sprintf("go_ari_client/%s go-nsq/%s", util.BINARY_VERSION, nsq.VERSION)
+	cfg.MaxInFlight = maxInFlight
+
+	// create new consumer and attach to lookupd
+	consumer, err := nsq.NewConsumer(config.Application, config.Channel, cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-  newHandler, in := GenHandler()
-  consumer.AddHandler(nsq.HandlerFunc(newHandler))
-  out := make(chan *nv.NV_Event)
-  nv.Init(in, out)
-
-  err = consumer.ConnectToNSQLookupds(config.LookupdHttpAddress)
+	err = consumer.ConnectToNSQLookupds(config.LookupdHttpAddress)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-  go ProcessMessage(out)
-  
-  for {
+	// generate a handler for processing inbound events
+	processEvent, inboundEvents := GenHandler()
+
+	// process events as they come off the bus
+	consumer.AddHandler(nsq.HandlerFunc(processEvent))
+
+	// create channel to place parsed events onto
+	parsedEvents := make(chan *nv.NV_Event)
+
+	// create consumer that uses the inboundEvents and parses them onto the parsedEvents channel
+	nv.InitConsumer(inboundEvents, parsedEvents)
+
+	// start consuming the parsed events
+	go ConsumeEvents(parsedEvents)
+
+	// pull the events off the bus
+	for {
 		select {
 		case <-consumer.StopChan:
 			return
